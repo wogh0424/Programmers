da7b11473d1750289c1db6e7719cfc018660175f
wogh0424 <40976527+wogh0424@users.noreply.github.com>
2023-06-10 14:14:50 +0900

    [unrated] Title: 홀짝 구분하기, Time: 153.66 ms, Memory: 67.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -5,10 +5,7 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
-        if(n % 2 == 0) {
-        	System.out.println(n + " " + "is even");
-        }else {
-        	System.out.println(n + " " + "is odd");
-        }
+                System.out.println(n % 2 == 0 ? n + " is even " : n + " is odd ");
+
     }
 }
